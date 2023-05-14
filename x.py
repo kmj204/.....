2a7d542d2e465fc5a92db63454813c04e549a4fb
kmj204 <133008090+kmj204@users.noreply.github.com>
2023-05-14 23:34:31 +0900

    Add files via upload

---
+++ x.py
@@ -0,0 +1,18 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hell world")
+Hell world
+>>> type(10)
+<class 'int'>
+>>> type(10.1)
+<class 'float'>
+>>> type(10+ 5j)
+<class 'complex'>
+>>> type("10")
+<class 'str'>
+>>> type (True)
+<class 'bool'>
+>>> type (False)
+<class 'bool'>
+>>> 10**3
+1000
